efc863567d0b1ec18c38a3b58d7760b6c10dc949
Cassius Jonus <vegetafan5@gmail.com>
2022-02-14 17:35:47 -0600

    Added a javadoc comment in Main.scala

--- src/main/scala/jonus/cassius/wordcounter/Main.scala
+++ src/main/scala/jonus/cassius/wordcounter/Main.scala
@@ -1,6 +1,12 @@
 package jonus.cassius.wordcounter
 
-
+/**
+ * 
+ * The entry point for the application. 
+ * 
+ * @author: Cassius jonus
+ * @version: 0.1.0
+ */ 
 object Main {
 
   def main(args: Array[String]): Unit = {
