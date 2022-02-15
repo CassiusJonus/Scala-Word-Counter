93e2d54bfc3fad79396bb7ea29e1be50d308618e
Cassius Jonus <vegetafan5@gmail.com>
2022-02-14 20:06:31 -0600

    Implemente the getWordsFrom(path) method in WordCounter.scala.

--- src/main/scala/jonus/cassius/wordcounter/WordCounter.scala
+++ src/main/scala/jonus/cassius/wordcounter/WordCounter.scala
@@ -1,6 +1,6 @@
 package jonus.cassius.wordcounter
 
-import scala.io.{Source, BufferedSource, File}
+import scala.io.{Source, BufferedSource, Codec}
 
 /** 
 * This object contains the logic for the application.
@@ -8,7 +8,17 @@ import scala.io.{Source, BufferedSource, File}
 
 object WordCounter:
 
-  def getWordsFrom(path: String): List[String] = {
+  def getWordsFrom(path: String): Either[String, List[String]] = {
+    try {
+      val fileSource: BufferedSource = Source.fromFile(path, Codec.UTF8)
+      Right(fileSource.getLines().flatMap(_.split("\\W+")))
+    }
+    catch {
+      case e: FileNotFoundException => Left(s"$path does not exist.")
+      case e: IOException => Left("An error occurred while trying to access the path specified. Perhaps the path is invalid.")
+    }
+    finally fileSource.close
+
     
   }
 
