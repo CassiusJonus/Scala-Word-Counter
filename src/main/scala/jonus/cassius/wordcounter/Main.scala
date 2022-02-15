3a71252f9b96737c646eb625c3843c58835743af
Cassius Jonus <vegetafan5@gmail.com>
2022-02-15 08:22:28 -0600

    Changed the name of getWordsFrom() to getLinesFrom().

--- src/main/scala/jonus/cassius/wordcounter/Main.scala
+++ src/main/scala/jonus/cassius/wordcounter/Main.scala
@@ -2,32 +2,30 @@ package jonus.cassius.wordcounter
 
 import scala.io.StdIn
 
+import scala.util.Either
 
-/**
- * 
- * The entry point for the application. 
- * 
- * @author: Cassius jonus
- * @version: 0.1.0
- */ 
+/** The entry point for the application.
+  *
+  * @author: Cassius jonus
+  * @version: 0.1.0
+  */
 object Main {
 
   def main(args: Array[String]): Unit = {
-   
+
     println("""
     |Welcome to the word counter application! Enter a path to a text file, 
     |to find out how many words it contains. Enter
     |
     """)
-   
-    val wordsOrError: Either[String, Iterator[String]] = WordCounter.getWordsFrom(getFilePath())
-   /* wordsOrError match {
+
+    val wordsOrError: Either[String, Iterator[String]] = WordCounter.getLinesFrom(getFilePath())
+    /* wordsOrError match {
      case Right(words) => println(s"The number of words in the file is ${WordCounter.count(words)}")
      case Left(error) => println(error)
    } */
   }
 
-  def getFilePath(): String = {
+  def getFilePath(): String =
     StdIn.readLine("Enter the path to a text file: ")
-  }
 }
--- src/main/scala/jonus/cassius/wordcounter/WordCounter.scala
+++ src/main/scala/jonus/cassius/wordcounter/WordCounter.scala
@@ -1,26 +1,25 @@
 package jonus.cassius.wordcounter
 
-import scala.io.{Source, BufferedSource, Codec, File}
+import scala.io.{BufferedSource, Codec, File, Source}
 import java.io.{FileNotFoundException, IOException}
 import java.io.File
-/** 
-* This object contains the logic for the application.
-*/
+
+/** This object contains the logic for the application.
+  */
 
 object WordCounter:
 
-  
-
-  def getWordsFrom(path: String): Either[String, Iterator[String]] = {
+  def getLinesFrom(path: String): Either[String, Iterator[String]] = {
     import scala.util.Either
-    
+
     try {
       val fileSource = Source.fromFile(path)(Codec.UTF8)
-      Right(fileSource.getLines().flatMap(_.split("\\W+")))
+      Right(fileSource.getLines())
     }
     catch {
       case e: FileNotFoundException => Left(s"$path does not exist.")
-      case e: IOException => Left("An error occurred while trying to access the path specified. Perhaps the path is invalid.")
+      case e: IOException =>
+        Left("An error occurred while trying to access the path specified. Perhaps the path is invalid.")
     }
   }
 
