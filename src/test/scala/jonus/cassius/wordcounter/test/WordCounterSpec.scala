31087f140bd4957e02efa0ef659f17015565e6c1
Cassius Jonus <vegetafan5@gmail.com>
2022-02-15 15:08:42 -0600

    Added two tests

--- src/test/scala/jonus/cassius/wordcounter/test/WordCounterSpec.scala
+++ src/test/scala/jonus/cassius/wordcounter/test/WordCounterSpec.scala
@@ -4,6 +4,8 @@ package test
 import scala.io.{BufferedSource, Codec, Source}
 import scala.util.Either
 
+import java.io.{FileNotFoundException, IOException}
+
 import org.scalatest.flatspec.AnyFlatSpec
 
 import jonus.cassius.wordcounter.WordCounter
@@ -14,5 +16,14 @@ class WordCounterSpec extends AnyFlatSpec {
     val emptyFile = Source.fromResource("empty.txt")(Codec.UTF8)
     assert(emptyFile.getLines.isEmpty)
     emptyFile.close()
+  }
+
+  "A path to a non-existent file" should "throw a FileNotFoundException" in {
+    assertThrows[FileNotFoundException](Source.fromFile("notFound.txt")(Codec.UTF8))
+  }
+
+  "A non-empty file" should "have a word count greater than zero" in {
+    val nonEmptyFile = Source.fromResource("words.txt")(Codec.UTF8)
+    assert(WordCounter.count(nonEmptyFile.getLines) > 0)
   }
 }
