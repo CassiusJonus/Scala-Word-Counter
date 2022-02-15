43e8b0d77d7ad71080a19269f9f6dab4b41b4e33
Cassius Jonus <vegetafan5@gmail.com>
2022-02-14 18:39:13 -0600

    main method in Main.scala

--- src/main/scala/jonus/cassius/wordcounter/Main.scala
+++ src/main/scala/jonus/cassius/wordcounter/Main.scala
@@ -14,5 +14,21 @@ object Main {
 
   def main(args: Array[String]): Unit = {
    
+    println("""
+    |Welcome to the word counter application! Enter a path to a text file, 
+    |to find out how many words it contains. Enter
+    |
+    """)
+   
+   val wordsOrError: Either[String, List[String]] = WordCounter.getWordsFrom(getFilePath())
+   wordsOrError match {
+     case Right(words) => println(s"The number of words in the file is ${WordCounter.count(words)}")
+     case Left(error) => println(error)
+   }
+  }
+
+  def getFilePath(): String = {
+    println("Enter the path to a text file: ")
+    StdIn.readLine
   }
 }
